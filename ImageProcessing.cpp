39f52228ff7e439f25cfd6ec8688c0a8cb04d19a
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2025-01-28 16:02:52 -0500

    Add files via upload

---
+++ ImageProcessing.cpp
@@ -0,0 +1,45 @@
+#include "ImageProcessing.h"
+#include <iostream> // to use cout and cin
+#include <cmath> // to use sqrt()
+
+using namespace std;
+
+// This is a reference function that copies the image from oData to rData.
+// See how arrays are utilized.
+void copyImage(int **oData, int **rData, int height, int width) {
+  for (int j=0; j<height; j++) {
+    for (int k=0; k<width; k++) {
+      rData[j][k] = oData[j][k];
+    }
+  }
+}
+
+void imgNegation(int **oData, int **rData, int height, int width) {
+  // TODO: write your code
+}
+
+void thresholding(int **oData, int **rData, int height, int width) {
+  // TODO: write your code
+}
+
+void histogramStretching(int **oData, int **rData, int height, int width) {
+  // TODO: write your code
+}
+
+void medianFilter(int **oData, int **rData, int height, int width) {
+  // TODO: write your code
+}
+
+void edgeDetect(int **oData, int **rData, int height, int width) {
+  // X-direction kernel
+  int Gx[3][3] = {{-1, 0, 1}, 
+                  {-2, 0, 2},
+                  {-1, 0, 1}};
+  
+  // Y-direction kernel
+  int Gy[3][3] = {{-1, -2, -1}, 
+                  { 0,  0,  0},
+                  { 1,  2,  1}};
+
+  // TODO: write your code
+}
